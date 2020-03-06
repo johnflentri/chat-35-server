adf4b0438581299d7f818daa0b69d40c84b20d75
johnflentri <jpflentri@gmail.com>
2020-03-06 15:40:07 +0100

    Added process.env.PORT option

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const Sse = require('json-sse')
 const cors = require('cors')
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
 
 // app.get('/', (req, res) => res.send('Hello World!'))
 
