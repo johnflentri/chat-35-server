c4cb59e8e38eac46c84c387460afedfea5b494c7
johnflentri <jpflentri@gmail.com>
2020-03-06 10:47:20 +0100

    Register POST /message

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const express = require('express')
+const Sse = require('json-sse')
 const app = express()
 const port = 4000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+const db = {}
+
+db.messages = []
+
+const parser = express.json()
+app.use(parser)
+
+const stream = new Sse()
+
+app.get('/stream', (request, response) => {
+  stream.init(request, response)
+})
+
+app.post(
+  '/message',
+  (request, response) => {
+    const { text } = request.body
+
+    db.messages.push(text)
+
+    response.send(text)
+
+    stream.send(text)
+
+    console.log('db test:', db)
+  }
+)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
